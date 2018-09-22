0b7955d5cc929f39c30956b4c55d51da64f8ede3
L1lith <lily@lillith.pw>
2018-09-22 06:46:56 -0600

    fix variable declaration

--- core/handleRequest.js
+++ core/handleRequest.js
@@ -9,7 +9,7 @@ function handleRequest(directory) {
   const renderer = new Renderer(directory)
   return asyncHandler(async (req, res, next) => {
     let path = url.parse(req.url).pathname
-    const ext = extname(path) || ".html"
+    let ext = extname(path) || ".html"
     path = removeExtensionFromPath(path)
     if (ext && !['.html', '.jsp'].includes(ext)) return next() // We don't handle other file extensions
     if (ext === '.jsp') ext = '.js' // Convert JSP (JavaScript Page) to actual JS extension
