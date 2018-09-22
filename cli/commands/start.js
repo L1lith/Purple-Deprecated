4d0a77af3d46fc55d98744376ef88d336955ab99
L1lith <lily@lillith.pw>
2018-09-22 02:54:14 -0600

    work on cli

--- cli/commands/start.js
+++ cli/commands/start.js
@@ -1,12 +1,12 @@
 const validateArgs = require('../validateArgs')
 const {access} = require('fs-extra')
+const {dirname} = require('path')
 
 const args = validateArgs({
   "directory": {
     aliases: ["d", "dir"],
-    sanitize: String,
-    required: true
+    sanitize: String
   }
 })
 
-const {directory} = args
+const directory = args.directory || process.cwd()
--- cli/index.js
+++ cli/index.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 const command = require('yargs').argv._[0]
 const {sanitize, F} = require('sandhands')
+const {join} = require('path')
+const {accessSync} = require('fs')
 
 if (!command) throw new Error("Missing Command Argument!")
 sanitize(command, F(String).regex(/^[a-zA-Z]+$/))
 
+const commandPath = join(__dirname, './commands/'+command+".js")
+
 try {
-  require('./commands/'+command+".js")
+  accessSync(commandPath)
 } catch(err) {
+  console.log(err)
   throw new Error(`The command ${command} does not exist. Try "purpl help"`)
 }
+
+require(commandPath)
