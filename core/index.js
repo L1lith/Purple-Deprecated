5766f2ad3b37fa04d8afd00634e73837ea59f9bb
L1lith <lily@lillith.pw>
2018-09-22 03:38:53 -0600

    add default port

--- core/index.js
+++ core/index.js
@@ -1,7 +1,7 @@
 const {join} = require('path')
 const createServer = require('./createServer')
 
-async function runPurpl(directory, {port}) {
+async function runPurpl(directory, {port=8040}) {
   const app = await createServer(directory)
   if (!port) throw new Error("Missing Port")
   await app.listen(port)
