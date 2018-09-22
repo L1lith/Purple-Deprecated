2dcbde7dc5e5e49b7e284269754d2a768ba08f61
L1lith <lily@lillith.pw>
2018-09-21 19:28:30 -0600

    create example purpl project to develop with

---
+++ example/pages/docs/index.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+
+class Docs extends Component {
+  return <p>Welcome to the docs!</p>
+}
+
+export default Docs
---
+++ example/pages/home.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+  return <p>Example Purple Project</p>
+}
+
+export default Home
