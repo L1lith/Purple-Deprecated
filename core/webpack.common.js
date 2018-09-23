f38da4c5d1759a4a2caef5f30a6626ee02f11f55
L1lith <lily@lillith.pw>
2018-09-22 23:42:59 -0600

    implement babel

--- core/webpack.common.js
+++ core/webpack.common.js
@@ -1,3 +1,15 @@
 module.exports = {
-  mode: process.env.NODE_ENV || "development"
+  mode: process.env.NODE_ENV || "development",
+  module: {
+    rules: [{
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-env']
+        }
+      }
+    }]
+  }
 }
