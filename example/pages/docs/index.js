d9ab1097d387b8ef86129799d52058127a4342df
L1lith <lily@lillith.pw>
2018-09-23 12:24:49 -0600

    fix bugs in the example

--- example/pages/docs/index.js
+++ example/pages/docs/index.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react'
 
 class Docs extends Component {
-  return <p>Welcome to the docs!</p>
+  render() {
+    return <p>Welcome to the docs!</p>
+  }
 }
 
 export default Docs
--- example/pages/index.js
+++ example/pages/index.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 
 class Home extends Component {
-  return (
-    <div>
-      <p>Example Purple Project</p>
-      <img src="/sitting_cow.jpg"/>
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        <p>Example Purple Project</p>
+        <img src="/sitting_cow.jpg"/>
+      </div>
+    )
   }
+}
 
 export default Home
