aae52076b3fd14897ac0be3c36773db789bd8397
L1lith <lily@lillith.pw>
2018-09-24 19:16:55 -0600

    work on the purple init command

--- cli/commands/init.js
+++ cli/commands/init.js
@@ -3,6 +3,7 @@ const mkdirp = require('mkdirp')
 const {lstatSync, readdirSync} = require('fs')
 const {join} = require('path')
 const {ncp} = require('ncp')
+const {exec} = require('child_process')
 
 const templateDir = join(__dirname, '../../template')
 
@@ -17,13 +18,21 @@ const args = validateArgs({
 let directory = args.directory || args._[0] || "./Purple-Project"
 if (directory.startsWith('./')) directory = join(process.cwd(), directory)
 mkdirp.sync(directory)
-if (!lstatSync(directory).isDirectory()) throw new Error("Path is not a directory")
-if (readdirSync(directory).length > 0) throw new Error("You must specify a non empty directory with the -d flag")
+if (!lstatSync(directory).isDirectory()) throw "Path is not a directory"
+if (readdirSync(directory).length > 0) throw "You must specify a non empty directory with the -d flag"
 
+console.log("Cloning Template")
 ncp(templateDir, directory, err => {
   if (err) {
     console.log(err)
     process.exit(1)
   }
-  console.log("Purple Project Created!")
+  console.log("Installing Dependencies")
+  exec('npm install', {cwd: directory}, err => {
+    if (err) {
+      console.log(err)
+      process.exit(1)
+    }
+    console.log(`Purple Project Created in /${directory}!`)
+  })
 })
