37c062e952d3eeda80a0d351db7ea269206b9886
L1lith <lily@lillith.pw>
2018-09-27 00:19:46 -0600

    create path escaping

--- core/functions/convertPathToRegex.js
+++ core/functions/convertPathToRegex.js
@@ -8,7 +8,7 @@ const pathAliases = [
 ]
 
 function convertPathToRegex(path) {
-  path = path.split('#').filter((value, index) => index % 2  == 0).join('') // Allow Comments using the # Character.
+  path = path.split(notEscaped('#')).filter(str => str.length > 0).filter((value, index) => index % 2  == 0).join('') // Allow Comments using the # Character.
   let parts = path.split('/').map(part => replaceIn(part, pathAliases))
 
   return new RegExp(`^${parts.join('\/')}$`, 'i')
@@ -17,7 +17,11 @@ function convertPathToRegex(path) {
 function replaceIn(string, searches) {
   if (searches.length < 1) return escapeForRegex(string)
   const [alias, regex] = searches[0]
-  return string.split(alias).map(part => replaceIn(part, searches.slice(1))).join(regex)
+  return string.split(notEscaped(alias)).map(part => replaceIn(part, searches.slice(1))).join(regex)
+}
+
+function notEscaped(string) {
+  return new RegExp('(^|[^\\\\])' + escapeForRegex(string), 'i')
 }
 
 module.exports = convertPathToRegex
