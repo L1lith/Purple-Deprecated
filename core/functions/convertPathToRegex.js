78d15b93cfd0e9c9856c3b2c0621c5977527f271
L1lith <lily@lillith.pw>
2018-09-26 23:28:37 -0600

    improve avaliable path wildcards

--- core/functions/convertPathToRegex.js
+++ core/functions/convertPathToRegex.js
@@ -1,13 +1,22 @@
 const escapeForRegex = require("./escapeForRegex")
 
+const pathAliases = [
+  ["**+", ".+"],
+  ["*+", "[^/]+"],
+  ["**", ".*"],
+  ["*", "[^/]*"],
+]
+
 function convertPathToRegex(path) {
-  let parts = path.split('/')
-  parts = parts.map(part => {
-    if (part.length < 1) return ''
-    return part.split('**').map(str => str.split('*').map(escapeForRegex).join("[^/]*")).join(".*")
-  })
+  let parts = path.split('/').map(part => replaceIn(part, pathAliases))
 
   return new RegExp(`^${parts.join('\/')}$`, 'i')
+}
+
+function replaceIn(string, searches) {
+  if (searches.length < 1) return escapeForRegex(string)
+  const [alias, regex] = searches[0]
+  return string.split(alias).map(part => replaceIn(part, searches.slice(1))).join(regex)
 }
 
 module.exports = convertPathToRegex
