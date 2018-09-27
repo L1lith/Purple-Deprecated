70b858960bebb27d3163fde8c5c1602d03f29074
L1lith <lily@lillith.pw>
2018-09-26 23:41:06 -0600

    add colors to cli

--- cli/commands/init.js
+++ cli/commands/init.js
@@ -4,6 +4,7 @@ const {lstatSync, readdirSync} = require('fs')
 const {join} = require('path')
 const {ncp} = require('ncp')
 const {exec} = require('child_process')
+const {magenta, cyan, green, yellow} = require('chalk')
 
 const templateDir = join(__dirname, '../../template')
 
@@ -16,23 +17,24 @@ const args = validateArgs({
 
 
 let directory = args.directory || args._[0] || "./Purple-Project"
+let directoryDisplayName = directory
 if (directory.startsWith('./')) directory = join(process.cwd(), directory)
 mkdirp.sync(directory)
 if (!lstatSync(directory).isDirectory()) throw "Path is not a directory"
 if (readdirSync(directory).length > 0) throw "You must specify a non empty directory with the -d flag"
 
-console.log("Cloning Template")
+console.log(magenta(`Creating a new Purple Project!`))
 ncp(templateDir, directory, err => {
   if (err) {
     console.log(err)
     process.exit(1)
   }
-  console.log("Installing Dependencies")
+  console.log(cyan("Installing Dependencies..."))
   exec('npm install', {cwd: directory}, err => {
     if (err) {
       console.log(err)
       process.exit(1)
     }
-    console.log(`Purple Project Created in /${directory}!`)
+    console.log(cyan(`Purple Project Created in ${green(directoryDisplayName)}!\nTo get started ${green("cd")} into the directory and run "${green("npm start")}".`))
   })
 })
